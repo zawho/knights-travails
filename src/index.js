eb944ae9b826cb0e57aaaad28da5838ed79e9be8
zawho <zthooper2@gmail.com>
2024-06-18 09:22:48 +0800

    Add basic knightMoves func with helper funcs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Board from "./board";
-import allMoves from "./moves-list";
+import { allMoves, KnightMoves } from "./moves-list";
 
 // Create board.
 const testBoard = new Board();
@@ -11,9 +11,14 @@ const startKnight = testBoard.placeKnight([3, 3]);
 const knightLoc = testBoard.findKnight(testBoard.board);
 
 // Return and print array of all possible moves.
-console.log(testBoard.displayMoves(knightLoc));
+// console.log(testBoard.displayMoves(knightLoc));
 
 //Print chess board.
-console.table(testBoard.board);
+// console.table(testBoard.board);
 
-allMoves();
+// allMoves();
+
+KnightMoves([3, 3], [4, 3]);
+
+// clean up needed
+// re org needed
--- src/moves-list.js
+++ src/moves-list.js
@@ -1,38 +1,65 @@
 import Board from "./board";
 
 function createList(board) {
-    const movesList = [];
+  const movesList = [];
 
-    board.forEach((item) => {
-      for (let i = 0; i < item.length; i++) {
-        movesList.push([]);
-      }
-    });
+  board.forEach((item) => {
+    for (let i = 0; i < item.length; i++) {
+      movesList.push([]);
+    }
+  });
 
-    return movesList;
+  return movesList;
+}
+
+function fillBoard(boardVar) {
+  let count = -1;
+  boardVar.forEach((arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      count += 1;
+      arr[i] = count;
+    }
+  });
+}
+
+function allMoves() {
+  const testBoard = new Board();
+  const movesList = createList(testBoard.board);
+
+  fillBoard(testBoard.board);
+
+  testBoard.board.forEach((arr) => {
+    const rowIndex = testBoard.board.indexOf(arr);
+    for (let i = 0; i < arr.length; i++) {
+      testBoard.findMoves([rowIndex, i], movesList[arr[i]]);
+    }
+  });
+
+  console.table(testBoard.board);
+  console.log(movesList);
+}
+
+function getValue(coords, boardVar) {
+ let coordsVal;
+ boardVar.forEach((arr) => {
+  const rowIndex = boardVar.indexOf(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (rowIndex === coords[0] && i === coords[1]) {
+      coordsVal = arr[i];
+    }
   }
+ });
+return coordsVal;
+}
 
-  function allMoves() {
-    const testBoard = new Board();
-    const movesList = createList(testBoard.board);
-    let testVar = -1;
+function KnightMoves(start, end) {
+  const chessBoard = new Board();
+  fillBoard(chessBoard.board);
 
-    testBoard.board.forEach((arr) => {
-      for (let i = 0; i < arr.length; i++) {
-        testVar += 1;
-        arr[i] = testVar;
-      }
-    })
+  const startVal = getValue(start, chessBoard.board);
+  const endVal = getValue(end, chessBoard.board);
 
-    testBoard.board.forEach((arr) => {
-      const rowIndex = testBoard.board.indexOf(arr);
-      for (let i = 0; i < arr.length; i++) {
-        testBoard.findMoves([rowIndex, i], movesList[arr[i]]);
-      }
-    })
+  console.table(chessBoard.board);
+}
 
-    console.table(testBoard.board);
-    console.log(movesList);
-  }
-
-  export default allMoves;
+export { allMoves, KnightMoves };
